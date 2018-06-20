045b5c4634cb4c0d805ab0546859839b5fd98cdd
Jared Rice Sr <39349252+JaredRiceSr@users.noreply.github.com>
2018-06-20 02:50:36 -0500

    Update Main.vue

--- src/components/header/Main.vue
+++ src/components/header/Main.vue
@@ -4,7 +4,7 @@
     <router-link
       :to="{ name: 'home' }"
       class="logo-container w-50px md:w-80px h-50px md:h-80px flex-none bg-red text-2xl xl:rounded-l-md flex justify-center items-center">
-      <img class="logo max-w-25px md:max-w-38px" src="@/assets/images/benchpay-logo.png" />
+      <img class="logo max-w-25px md:max-w-38px" src="@/assets/images/bench-testnet-logo.png" />
     </router-link>
 
     <div class="w-full relative hidden xl:flex">
