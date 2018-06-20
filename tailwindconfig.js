d584ed83d239780870195ac0a1d2a4bebd54199e
Jared Rice Sr <39349252+JaredRiceSr@users.noreply.github.com>
2018-06-20 02:44:45 -0500

    Update tailwindconfig.js

--- tailwindconfig.js
+++ tailwindconfig.js
@@ -80,7 +80,7 @@ let colors = {
   'red-darkest': '#3b0d0c',
   'red-darker': '#621b18',
   'red-dark': '#cc1f1a',
-  red: '#EF192D', // UPDATED
+  red: '#50E3C2', // UPDATED
   'red-light': '#ef5753',
   'red-lighter': '#f9acaa',
   'red-lightest': '#fcebea',
